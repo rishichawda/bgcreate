a3ec2507fb706b56dc7859814152c27a61c13039
Rishi Kumar <chawda.rishii@gmail.com>
2018-05-25 12:31:52 +0530

    add mouse event

--- js/paint.js
+++ js/paint.js
@@ -1,40 +1,38 @@
 var pencil_color = '#FFFFFF';
-var paint_canvas = false;
-var context;
 
 $(document).on('build','#paint-canvas',function(){ 
-    context = document.getElementById('paint-canvas').getContext('2d');
+    var paint_canvas = false;
+    var context = this.getContext('2d');
+    $('#paint-canvas').on('mousedown',function(e){
+        var x_pos = e.pageX - this.offsetLeft;
+        var y_pos = e.pageY - this.offsetTop;
+        context.strokeStyle = pencil_color;
+        context.lineWidth = 7;
+        paint_canvas = true;
+        context.beginPath();
+        context.moveTo(x_pos,y_pos);
+    });
+    
+    $('#paint-canvas').on('mouseup',function(e){
+        paint_canvas = false;
+        context.stroke();
+    });
+
+    $('#paint-canvas').on('mouseleave',function(e){
+        paint_canvas = false;
+        context.stroke();
+    });
+    
+    $('#paint-canvas').on('mousemove',function(e){
+        if(paint_canvas) { 
+            var x_pos = e.pageX - this.offsetLeft;
+            var y_pos = e.pageY - this.offsetTop;
+            context.lineTo(x_pos,y_pos);
+        }
+    });
  });
 
 function select_pencil_color() { 
     pencil_color = '#' + $('#pencil-color')[0].value;
-    console.log(pencil_color);
  }
 
-$(document).on('mousedown','#paint-canvas',function(e){
-    var x_pos = e.pageX - this.offsetLeft;
-    var y_pos = e.pageY - this.offsetTop;
-    context.strokeStyle = pencil_color;
-    context.lineJoin = "round";
-    context.lineWidth = 7;
-    paint_canvas = true;
-    console.log('start painting');
-    context.beginPath();
-    context.moveTo(x_pos,y_pos);
-    // console.log(context);
-});
-
-$(document).on('mouseup','#paint-canvas',function(e){
-    paint_canvas = false;
-    context.stroke();
-    console.log('stopped painting');
-});
-
-$(document).on('mousemove','#paint-canvas',function(e){
-    if(paint_canvas) { 
-        console.log('painting');
-        var x_pos = e.pageX - this.offsetLeft;
-        var y_pos = e.pageY - this.offsetTop;
-        context.lineTo(x_pos,y_pos);
-    }
-});
