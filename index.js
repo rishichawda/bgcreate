82fb42ae9e5b4213fd15980f1772235279cc2991
Rishi Kumar <chawda.rishii@gmail.com>
2018-11-19 23:40:53 +0530

    feat(src): add menubar options

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { initializeIcons } from '@uifabric/icons';
+import MenuBar from './src/menubar';
 import './index.scss';
 
 initializeIcons();
 
 const App = () => (
   <div>
+    <MenuBar />
       Hello world
   </div>
 );
---
+++ src/menubar.js
@@ -0,0 +1,105 @@
+import React, { Component } from 'react';
+import { CommandBar } from 'office-ui-fabric-react/lib/CommandBar';
+import About from './about';
+
+class MenuBar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showModal: false,
+    };
+  }
+
+  // Data for CommandBar
+  getItems = () => [
+    {
+      key: 'newItem',
+      name: 'New',
+      cacheKey: 'myCacheKey', // changing this key will invalidate this items cache
+      iconProps: {
+        iconName: 'Add',
+      },
+      ariaLabel: 'Use left and right arrow keys to navigate',
+    },
+    {
+      key: 'share',
+      name: 'Share',
+      iconProps: {
+        iconName: 'Share',
+      },
+      disabled: true,
+      onClick: () => console.log('Share'),
+    },
+    {
+      key: 'download',
+      name: 'Download',
+      iconProps: {
+        iconName: 'Download',
+      },
+      onClick: () => console.log('Download'),
+    },
+  ];
+
+   getOverlflowItems = () => [
+     {
+       key: 'move',
+       name: 'Move to...',
+       onClick: () => console.log('Move to'),
+       iconProps: {
+         iconName: 'MoveToFolder',
+       },
+     },
+     {
+       key: 'copy',
+       name: 'Copy to...',
+       onClick: () => console.log('Copy to'),
+       iconProps: {
+         iconName: 'Copy',
+       },
+     },
+     {
+       key: 'rename',
+       name: 'Rename...',
+       onClick: () => console.log('Rename'),
+       iconProps: {
+         iconName: 'Edit',
+       },
+     },
+   ];
+
+   getFarItems = () => [
+     {
+       key: 'info',
+       name: 'About',
+       iconProps: {
+         iconName: 'Info',
+       },
+       onClick: () => this.showModal(),
+     },
+   ];
+
+   showModal = () => {
+     this.setState({ showModal: true });
+   };
+
+  closeModal = () => {
+    this.setState({ showModal: false });
+  };
+
+  render() {
+    const { showModal } = this.state;
+    return (
+      <div>
+        <About showModal={showModal} closeModal={this.closeModal} />
+        <CommandBar
+          items={this.getItems()}
+          overflowItems={this.getOverlflowItems()}
+          farItems={this.getFarItems()}
+          ariaLabel="Use left and right arrow keys to navigate between commands"
+        />
+      </div>
+    );
+  }
+}
+
+export default MenuBar;
