d6c3988e046eeea0bb8123e8c7f9b2269b14d432
Rishi Kumar <chawda.rishii@gmail.com>
2018-11-20 01:09:40 +0530

    feat(src,dist): add canvas, jquery and old code

--- src/toolbar.js
+++ src/toolbar.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { DefaultButton } from 'office-ui-fabric-react/lib/Button';
 import { Panel, PanelType } from 'office-ui-fabric-react/lib/Panel';
+import ToolbarOptions from './toolbarOptions';
 
 class Toolbar extends React.Component {
   render() {
@@ -12,9 +13,8 @@ class Toolbar extends React.Component {
           type={PanelType.smallFixedNear}
           // tslint:disable-next-line:jsx-no-lambda
           onDismiss={closePanel}
-          headerText="Panel - Small, left-aligned, fixed"
         >
-          <span>Content goes here.</span>
+          <ToolbarOptions />
         </Panel>
       </div>
     );
---
+++ src/toolbarOptions.js
@@ -0,0 +1,13 @@
+import * as React from 'react';
+
+class ToolbarOptions extends React.Component {
+
+  render() {
+    return (
+      <div className="toolbar">
+      </div>
+    );
+  }
+}
+
+export default ToolbarOptions;
