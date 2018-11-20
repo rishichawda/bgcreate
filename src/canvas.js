e5bade5ccc2d3c15e80c4686a0e64418d6c73c28
Rishi Kumar <chawda.rishii@gmail.com>
2018-11-20 21:45:33 +0530

    fix(canvas): fit canvas to page dimensions

--- src/canvas.js
+++ src/canvas.js
@@ -1,10 +1,19 @@
 import React, { Component } from 'react';
 
 class Canvas extends Component {
+  constructor(props) {
+    super(props);
+    this.canvasRef = React.createRef();
+  }
+
   render() {
     return (
       <div id="particles-js">
-        {/* <canvas class="particles-js-canvas-el" style="height: 100%;width: 100%;"></canvas> */}
+        <canvas
+          ref={this.canvasRef}
+          className="particles-js-canvas-el"
+          style={{ height: '100%', width: '100%' }}
+        />
       </div>
     );
   }
