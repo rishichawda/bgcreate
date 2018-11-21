6ed0c3ef32ecd4ece6fb3e4730315943431d777b
Rishi Kumar <chawda.rishii@gmail.com>
2018-11-21 23:05:27 +0530

    feat(src): add download dialog, minor source refactor

---
+++ src/index.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+import MenuBar from './menubar';
+import Toolbar from './toolbar';
+import store from './store';
+import Canvas from './components/canvas/index.js';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      toolbar: false,
+      modal: false,
+    };
+  }
+
+    openToolbar = () => {
+      this.setState({
+        toolbar: true,
+      });
+    };
+
+    closeModal = () => {
+      this.setState({
+        modal: false,
+      });
+    };
+
+    openModal = () => {
+      this.setState({
+        modal: true,
+      });
+    };
+
+    closeToolbar = () => {
+      this.setState({
+        toolbar: false,
+      });
+    };
+
+    render() {
+      const { toolbar, modal } = this.state;
+      return (
+        <Provider store={store}>
+          <div>
+            <MenuBar showModal={this.openModal} showToolbar={this.openToolbar} />
+            <Toolbar showPanel={toolbar} closePanel={this.closeToolbar} />
+            <Canvas showModal={modal} closeModal={this.closeModal} />
+          </div>
+        </Provider>
+      );
+    }
+}
+
+export default App;
