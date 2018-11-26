0352c50e2e45925d64cb5c9ed20b506712bb0efa
rishichawda <chawda.rishii@gmail.com>
2018-11-26 13:16:47 +0530

    fix(utils/generator): fix bg issue on downloading image with particles

--- src/utils/generator.js
+++ src/utils/generator.js
@@ -8,11 +8,11 @@ const generateDownloadLink = (image) => {
 export default (canvasBg, mode) => {
   const finalimage = document.createElement('canvas');
   const finalimageCanvascontext = finalimage.getContext('2d');
-  finalimageCanvascontext.fillStyle = canvasBg;
   if (mode === 'particles') {
     const canvas = document.getElementsByTagName('canvas')[0];
     finalimage.width = canvas.width;
     finalimage.height = canvas.height;
+    finalimageCanvascontext.fillStyle = canvasBg;
     finalimageCanvascontext.fillRect(0, 0, canvas.width, canvas.height);
     finalimageCanvascontext.drawImage(canvas, 0, 0);
     generateDownloadLink(finalimage);
@@ -20,6 +20,7 @@ export default (canvasBg, mode) => {
   }
   finalimage.width = 2000;
   finalimage.height = 2000;
+  finalimageCanvascontext.fillStyle = canvasBg;
   finalimageCanvascontext.fillRect(0, 0, 2000, 2000);
   generateDownloadLink(finalimage);
 };
