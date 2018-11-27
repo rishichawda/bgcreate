e16aa27aa6f1b237dfac96171f70934c946c400b
Rishi Kumar <chawda.rishii@gmail.com>
2018-11-27 23:03:47 +0530

    fix(utils/generator): fix download image size, add retina support

--- src/utils/generator.js
+++ src/utils/generator.js
@@ -1,27 +1,42 @@
 const generateDownloadLink = (image) => {
   const a = document.createElement('a');
-  a.href = image.toDataURL('image/jpeg').replace('image/jpeg', 'image/octet-stream');
+  a.href = image
+    .toDataURL('image/jpeg')
+    .replace('image/jpeg', 'image/octet-stream');
   a.download = 'bgGeneratorImage.jpg';
   document.body.appendChild(a);
   a.click();
 };
 
+const getViewportDimensions = (canvas) => {
+  const width = Math.max(
+    document.documentElement.clientWidth,
+    window.innerWidth || 0,
+    window.devicePixelRatio > 1
+      ? canvas.offsetWidth * window.devicePixelRatio
+      : canvas.offsetWidth,
+  );
+  const height = Math.max(
+    document.documentElement.clientHeight,
+    window.innerHeight || 0,
+    window.devicePixelRatio > 1
+      ? canvas.offsetHeight * window.devicePixelRatio
+      : canvas.offsetHeight,
+  );
+  return { width, height };
+};
+
 export default (canvasBg, mode) => {
   const finalimage = document.createElement('canvas');
   const finalimageCanvascontext = finalimage.getContext('2d');
-  if (mode === 'particles') {
-    const canvas = document.getElementsByTagName('canvas')[0];
-    finalimage.width = canvas.width;
-    finalimage.height = canvas.height;
-    finalimageCanvascontext.fillStyle = canvasBg;
-    finalimageCanvascontext.fillRect(0, 0, canvas.width, canvas.height);
-    finalimageCanvascontext.drawImage(canvas, 0, 0);
-    generateDownloadLink(finalimage);
-    return;
-  }
-  finalimage.width = 2000;
-  finalimage.height = 2000;
+  const canvas = document.getElementsByTagName('canvas')[0];
+  const { width, height } = getViewportDimensions(canvas);
+  finalimage.width = width;
+  finalimage.height = height;
   finalimageCanvascontext.fillStyle = canvasBg;
-  finalimageCanvascontext.fillRect(0, 0, 2000, 2000);
+  finalimageCanvascontext.fillRect(0, 0, width, height);
+  if (mode === 'particles') {
+    finalimageCanvascontext.drawImage(canvas, 0, 0);
+  }
   generateDownloadLink(finalimage);
 };
