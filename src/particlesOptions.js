c9031a15eaaedfa715ded3abe03fbf6d0c956895
Rishi Kumar <chawda.rishii@gmail.com>
2018-11-28 00:05:36 +0530

    feat(src/particlesOptions): add options to change and add multiple particle types to canvas

--- src/particlesOptions.js
+++ src/particlesOptions.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
-import { CommandBarButton, AnimationClassNames } from 'office-ui-fabric-react';
+import {
+  CommandBarButton, AnimationClassNames, Dropdown,
+} from 'office-ui-fabric-react';
+import { updateParticlesShape } from './utils/particles';
 
 class ParticlesOptions extends Component {
   constructor(props) {
     super(props);
     this.state = {
       collapse: true,
+      selected: ['circle'],
     };
   }
 
@@ -15,8 +19,24 @@ class ParticlesOptions extends Component {
     }));
   }
 
+  onChangeTypes = (_, options) => {
+    let { selected } = this.state;
+    if (selected.indexOf(options.key) !== -1) {
+      selected = selected.filter(val => val !== options.key);
+      this.setState({
+        selected,
+      });
+    } else {
+      selected = [...selected, options.key];
+      this.setState({
+        selected,
+      });
+    }
+    updateParticlesShape(selected);
+  }
+
   render() {
-    const { collapse } = this.state;
+    const { collapse, selected } = this.state;
     const particlesConfigProps = {
       'data-toolbar-option': 'particles-config',
       iconProps: { iconName: collapse ? 'ChevronDown' : 'ChevronUp' },
@@ -27,8 +47,22 @@ class ParticlesOptions extends Component {
       <div>
         <CommandBarButton {...particlesConfigProps} />
         {!collapse ? (
-          <div className={`${AnimationClassNames.scaleUpIn100}`}>
-              Particles options
+          <div className={`${AnimationClassNames.scaleUpIn100} particles-config-collapse`}>
+            <Dropdown
+              placeholder="Select particle types"
+              label="Particle types:"
+              id="particles-type-dropdown"
+              defaultSelectedKeys={selected}
+              onChange={this.onChangeTypes}
+              multiSelect
+              options={[
+                { key: 'circle', text: 'Circle' },
+                { key: 'edge', text: 'Edge' },
+                { key: 'triangle', text: 'Triangle' },
+                { key: 'polygon', text: 'Polygon', disabled: true },
+                { key: 'star', text: 'Star' },
+              ]}
+            />
           </div>
         ) : null}
       </div>
--- src/utils/particles.js
+++ src/utils/particles.js
@@ -18,3 +18,13 @@ export function unloadParticles() {
   canvas.appendChild(newcanvas);
   pJSDom = [];
 }
+
+export function updateParticlesShape(shape) {
+  pJSDom[0].pJS.fn.particlesEmpty();
+  if (typeof shape === 'object') {
+    for (let i = 0; i < shape.length; i += 1) {
+      pJSDom[0].pJS.particles.shape.type = shape[i];
+      pJSDom[0].pJS.fn.particlesCreate();
+    }
+  }
+}
