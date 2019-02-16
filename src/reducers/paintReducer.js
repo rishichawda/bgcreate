857323a44e0442553acbee65f95862319f9c842c
rishichawda <chawda.rishii@gmail.com>
2019-02-16 22:54:26 +0530

    fix(paint-canvas): sync linewidth

--- src/reducers/paintReducer.js
+++ src/reducers/paintReducer.js
@@ -1,4 +1,4 @@
-import { BRUSH_COLOR } from '../shared/actionTypes';
+import { BRUSH_COLOR, LINE_WIDTH } from '../shared/actionTypes';
 
 const INITIAL_STATE = {
   brushCol: '#000000',
@@ -9,6 +9,8 @@ export default function (state = INITIAL_STATE, action) {
   switch (action.type) {
     case BRUSH_COLOR:
       return { ...state, brushCol: action.payload };
+    case LINE_WIDTH:
+      return { ...state, lineWidth: action.payload };
     default:
       return state;
   }
--- src/shared/actionTypes.js
+++ src/shared/actionTypes.js
@@ -1,3 +1,4 @@
 export const BG_UPDATE = 'bg::update';
 export const MODE_UPDATE = 'mode::update';
 export const BRUSH_COLOR = 'update::brush::col';
+export const LINE_WIDTH = 'update::brush::width';
