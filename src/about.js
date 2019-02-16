22325c559630d0b455918ea6e06c7e1b0292517a
rishichawda <chawda.rishii@gmail.com>
2019-02-16 23:04:21 +0530

    fix(src): fix lint errors and update proptypes

--- src/about.js
+++ src/about.js
@@ -1,7 +1,7 @@
 // @codepen
 import * as React from 'react';
+import PropTypes from 'prop-types';
 import { Modal } from 'office-ui-fabric-react/lib/Modal';
-import { DefaultButton } from 'office-ui-fabric-react/lib/Button';
 import { Icon } from 'office-ui-fabric-react/lib/Icon';
 
 const About = ({ showModal, closeModal }) => (
@@ -20,18 +20,23 @@ const About = ({ showModal, closeModal }) => (
       </div>
       <div id="subtitleId" className="modal-body">
         <p>
-        Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+          {`Lorem Ipsum is simply dummy text of the printing and typesetting industry.
         Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
         when an unknown printer took a galley of type and scrambled it to make a type
         specimen book. It has survived not only five centuries, but also the leap
         into electronic typesetting, remaining essentially unchanged. It was
         popularised in the 1960s with the release of Letraset sheets containing
         Lorem Ipsum passages, and more recently with desktop publishing software
-        like Aldus PageMaker including versions of Lorem Ipsum.
+        like Aldus PageMaker including versions of Lorem Ipsum.`}
         </p>
       </div>
     </Modal>
   </div>
 );
+
+About.propTypes = {
+  showModal: PropTypes.bool.isRequired,
+  closeModal: PropTypes.func.isRequired,
+};
 
 export default About;
